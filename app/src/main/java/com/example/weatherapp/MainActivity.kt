69672195f8e7ad327e81aa76224e9e9476d15451
tiamabderezai <pass>
2021-11-03 20:24:05 -0400

    MainActivity completed

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.ActionBar
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etCity: TextInputEditText
+    private lateinit var btnLookup: MaterialButton
+    val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val actionBar: ActionBar? by lazy { supportActionBar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupUI()
+    }
+
+    fun setupUI() {
+        actionBar?.hide()
+        setContentView(binding.root)
+        etCity = binding.etCityName
+        btnLookup = binding.btnLookup
     }
 }
